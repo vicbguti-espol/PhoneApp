4c181f53b13655f82378f40877144af8b8cc42f3
vicbguti-espol <vicbguti@espol.edu.ec>
2023-11-11 19:55:51 -0500

    Fix contactList issues

--- app/src/main/java/model/attributes/reminders/prueba.java
+++ app/src/main/java/model/attributes/reminders/prueba.java
@@ -1,27 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package model.attributes.reminders;
-
-import java.text.ParseException;
-import java.time.LocalDate;
-
-/**
- *
- * @author naomi
- */
-public class prueba {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) throws ParseException {
-         
-         LocalDate fechaEspecifica = LocalDate.of(2003, 11, 13);
-        System.out.println(Reminder.calculateRemainingDays(fechaEspecifica));
-       
-        
-    }
-    
-}
+///*
+// * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+// * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+// */
+//package model.attributes.reminders;
+//
+//import java.text.ParseException;
+//import java.time.LocalDate;
+//
+///**
+// *
+// * @author naomi
+// */
+//public class prueba {
+//
+//    /**
+//     * @param args the command line arguments
+//     */
+//    public static void main(String[] args) throws ParseException {
+//         
+//         LocalDate fechaEspecifica = LocalDate.of(2003, 11, 13);
+//        System.out.println(Reminder.calculateRemainingDays(fechaEspecifica));
+//       
+//        
+//    }
+//    
+//}
--- app/src/main/java/model/user/MobilePhone.java
+++ app/src/main/java/model/user/MobilePhone.java
@@ -7,29 +7,29 @@ import model.contacts.Contact;
 import model.serialization.SerializationUtil;
 
 public class MobilePhone {
-    private static List<Contact> contactList = new ArrayList<>();
-
+    private static List<Contact> contactList;
     static final String contactListPath = "ser/contactList.ser";
     
     public static void addContact(Contact c) throws IOException{
-        contactList.add(c);
+        if (contactList == null) initContactList();
+        contactList.add(c);    
         SerializationUtil.serialize(contactList, contactListPath);
+    }
+    
+    public static void initContactList(){
+        try {
+            contactList =
+                    (ArrayList<Contact>) SerializationUtil.
+                            deserialize(contactListPath);
+        } catch (Exception e){
+            e.printStackTrace();
+            contactList = new ArrayList<>();
+        }
     }
     
     public static List<Contact> getContactList(){
         if (contactList == null){
-            try {
-                contactList =
-                        (ArrayList<Contact>) SerializationUtil.
-                                deserialize(contactListPath);
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            } catch (ClassNotFoundException ex) {
-                ex.printStackTrace();
-            } catch (Exception e){
-                e.printStackTrace();
-                contactList = new ArrayList<>();
-            }
+            initContactList();
         }
         return contactList;
     }
